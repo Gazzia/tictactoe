5e798c9e90f62a674cc3cfacb823d562234fcfc7
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-11-02 14:57:32 +0100

    Modif apparence grille, + isFull(), ~setValeur()

--- src/main/java/fr/jallain/Grille.java
+++ src/main/java/fr/jallain/Grille.java
@@ -19,21 +19,45 @@ public class Grille {
 			return ' ';
 	}
 
-	public void setValeur(int x, int y, char symbol) {
+	public boolean setValeur(int x, int y, char symbol) {
 		if (getValeur(x, y) == null) {
 			Pion pion = new Pion(symbol, x, y);
-			this.valeurs[x][y] = pion;
+			valeurs[x][y] = pion;
+			return true;
 		} else {
-			System.out.println("Deja un pion ici");
+			// Deja un pion ici
+			return false;
 		}
+
+	}
+
+	public boolean isFull() {
+		int total = 0;
+		for (Pion[] ligne : valeurs) {
+			for (Pion pion : ligne) {
+				if (pion != null)
+					total++;
+			}
+		}
+		return total == 9;
 	}
 
 	public String getDrawing() {
-		String separator = "---|---|---\n";
-		String line1 = " " + this.getSymbol(0, 0) + " | " + this.getSymbol(1, 0) + " | " + this.getSymbol(2, 0) + "\n";
-		String line3 = " " + this.getSymbol(0, 1) + " | " + this.getSymbol(1, 1) + " | " + this.getSymbol(2, 1) + "\n";
-		String line5 = " " + this.getSymbol(0, 2) + " | " + this.getSymbol(1, 2) + " | " + this.getSymbol(2, 2);
-		String result = line1 + separator + line3 + separator + line5;
+		String margin = "    ";
+		String subMargin = "   ";
+		String separator = margin + "  ---|---|---\n";
+
+		String line0 = margin + "x: 0   1   2   y\n\n";
+		String line1 = margin + subMargin + this.getSymbol(0, 0) + " | " + this.getSymbol(1, 0) + " | "
+				+ this.getSymbol(2, 0) + subMargin + "0\n";
+		String line3 = margin + subMargin + this.getSymbol(0, 1) + " | " + this.getSymbol(1, 1) + " | "
+				+ this.getSymbol(2, 1) + subMargin + "1\n";
+		String line5 = margin + subMargin + this.getSymbol(0, 2) + " | " + this.getSymbol(1, 2) + " | "
+				+ this.getSymbol(2, 2) + subMargin + "2";
+
+		String result = line0 + line1 + separator + line3 + separator + line5 + "\n";
+
 		return result;
 	}
+
 }
