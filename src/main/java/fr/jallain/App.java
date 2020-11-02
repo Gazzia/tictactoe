dc16919e7022b34fc962e5f449ae65b0cec0bb7c
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-11-02 14:56:18 +0100

    Amélioration du menu + bouton pour quit

--- src/main/java/fr/jallain/App.java
+++ src/main/java/fr/jallain/App.java
@@ -9,18 +9,25 @@ public final class App {
 		new Partie();
 	}
 
-	private static final void displayMainMenu() {
+	public static final void displayMainMenu() {
 		Utils.clearConsole();
-		System.out.println("---------------------------");
-		System.out.println("## PENDU - Menu Principal");
-		System.out.println("Que voulez-vous faire ?\n");
-		System.out.println("1) Nouvelle partie");
-		System.out.println("2) Quitter");
-		System.out.println("Choix: ");
+		System.out.println("_________________________________");
+		System.out.println("| x o x o x o x o x o x o x o x |");
+		System.out.println("---------------------------------");
+		System.out.println("|          * MORPION *          |");
+		System.out.println("|    Que voulez-vous faire ?    |");
+		System.out.println("---------------------------------");
+		System.out.println("|    1) NOUVELLE PARTIE         |");
+		System.out.println("|    2) QUITTER                 |");
+		System.out.println("L_______________________________I\n");
+		System.out.println("#: ");
 		String result = Utils.scanner.nextLine();
 		if (result.equals("1")) {
 			Utils.clearConsole();
 			newGame();
+		} else if (result.equals("2")) {
+			Utils.clearConsole();
+			System.exit(0);
 		} else {
 			Utils.clearConsole();
 			System.out.println("Je n'ai pas compris la commande..");
